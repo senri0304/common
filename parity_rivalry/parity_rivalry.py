a28820bc1e437e9989cd55101d181d313f08d176
hasegawa <43361198+senri0304@users.noreply.github.com>
2019-09-19 19:23:26 +0900

    Update parity_rivalry.py

--- parity_rivalry/parity_rivalry.py
+++ parity_rivalry/parity_rivalry.py
@@ -5,7 +5,6 @@ from collections import deque
 import pandas as pd
 import numpy as np
 
-
 # Prefernce
 #------------------------------------------------------------------------
 use_scr = 1
@@ -46,12 +45,14 @@ kud_list = [] # Store durations of key pressed
 cdt = [] #Store sum(kud), cumulative reaction time on a trial.
 mdt = []
 dtstd = []
+exitance = True
+n = 0
 
 # Load sound resource
 p_sound = pyglet.resource.media("button57.mp3", streaming = False)
 beep_sound = pyglet.resource.media("p01.mp3", streaming = False)
 
-#----------------------------------------------------------- 
+#----------- Core program following ---------------------------- 
 
 # A drawing polygon function
 class DrawStim():
@@ -83,21 +84,25 @@ class DrawStim():
 # A getting key response function
 class key_resp(object):
     def on_key_press(self, symbol, modifiers):
-        global tc, end_routine
-        if end_routine == False and symbol == key.SPACE:
+        global tc, exitance, trial_start
+        if exitance == False and symbol == key.SPACE:
             kd.append(time.time())
             tc = tc + 1
-        if end_routine == True and symbol == key.B:
-            end_routine = False
+        if exitance == True and symbol == key.B:
             p_sound.play()
-            pyglet.app.exit()
+            exitance = False
+            pyglet.clock.schedule_once(delete, 30.0)
+            pyglet.clock.schedule_once(Get_results, 31.0)
+            replace()
+            trial_start = time.time()
         if symbol == key.ESCAPE:
             win.close()
             pyglet.app.exit()
     def on_key_release(self, symbol, modifiers):
         global tc
-        if end_routine == False and symbol == key.SPACE:
+        if exitance == False and symbol == key.SPACE:
             ku.append(time.time())
+            tc = tc + 1
             tc = tc + 1
 resp_handler = key_resp()
 
@@ -119,17 +124,69 @@ def fixer():
     draw_objects.append(rfixw)
 
 # A end routine function
-def end_rou(dt):
-    global end_routine
-    end_routine = True
+def exit_routine(dt):
+    global exitance
+    exitance = True
     beep_sound.play()
-    # Display fixation
     fixer()
+    pyglet.app.exit()
+
+@win.event
+def on_draw():
+    # Refresh window
+    win.clear()
+    # 描画対象のオブジェクトを描画する
+    for draw_object in draw_objects:
+        draw_object.draw()
+
+# Remove stimulus
+def delete(dt):
+    global n, dl, trial_end
+    del draw_objects[:]
+    p_sound.play()
+    pyglet.clock.schedule_once(exit_routine, 30.0)
+    trial_end = time.time()
+
+# Add stimulus onto dispaly
+def replace():
+    draw_objects.append(test)
+    draw_objects.append(rival)
+
+def Get_results(dt):
+    global ku, kud, kd, kud_list, mdt, dtstd, n, tc, tcs ,trial_end, trial_start
+    ku.append(trial_start + 31.0)
+    while len(kd) > 0:
+        kud.append(ku.popleft() - kd.popleft() + 0) # list up key_press_duration
+    kud_list.append(str(kud))
+    c = sum(kud)
+    cdt.append(c)
+    tcs.append(tc)
+    if kud == []:
+        kud.append(0)
+    m = np.mean(kud)
+    d = np.std(kud)
+    mdt.append(m)
+    dtstd.append(d)
+    n += 1
+    print("--------------------------------------------------")
+    print("trial: " + str(n) + "/" + str(len(dat)))
+    print("start: " + str(trial_start))
+    print("end: " + str(trial_end))
+    print("key_pressed: " + str(kud))
+    print("transient counts: " + str(tc))
+    print("cdt: " + str(c))
+    print("mdt: " + str(m))
+    print("dtstd "+ str(d))
+    print("condition" + str(da))
+    print("--------------------------------------------------")
+    # Check the experiment continue or break
+    if n == dl:
+        pyglet.app.exit()
+
 
 # Store the start time
 start = time.time()
-
-fixer()
+win.push_handlers(resp_handler)
 
 #----------------- start loop -----------------------------
 # Get variables per trial from csv
@@ -152,78 +209,13 @@ for i in range(dl):
     list_c.append(colc)
     
     # Set up polygon for stimulus
-    cntrstm = DrawStim(5, am42, cntx + deg1*iso*test_x* colb, cnty, 0, 1, 0, 0)
-#    updot = DrawStim(5, 5, cntx + deg1*iso*-test_x - cola, cnty + am42/2 - 2.5, 0, 0, 0, 0)
-    downdot = DrawStim(5, am42*cola, cntx + deg1*iso*-test_x*colb, cnty , 90, 0, 0, 0)
-    
-    # Add stimulus onto dispaly
-    def replace(dt):
-        draw_objects.append(downdot)
-#        draw_objects.append(updot)
-        draw_objects.append(cntrstm)
-    
-    @win.event
-    def on_draw():
-        # Refresh window
-        win.clear()
+    rival = DrawStim(5, am42, cntx + deg1*iso*-colb, cnty, 0, 1, 0, 0)
+    test = DrawStim(5, am42*cola, cntx + deg1*iso*colb, cnty , 90, 0, 0, 0)
+
+    fixer()    
         
-        # 描画対象のオブジェクトを描画する
-        for draw_object in draw_objects:
-            draw_object.draw()
-    
-    # Event handler handlers
-    def set_handler(dt):
-        win.push_handlers(resp_handler)
-    def remove_handler(dt):
-        win.remove_handlers(resp_handler)
-    
-    # Remove stimulus
-    def delete(dt):
-        del draw_objects[:]
-        p_sound.play()
-        # Check the experiment continue or break
-        if i == dl - 1:
-            pyglet.app.exit()
-    
-    # Scheduling flow
-    pyglet.clock.schedule_once(remove_handler, 0.0)
-    pyglet.clock.schedule_once(replace, 1.0)
-    pyglet.clock.schedule_once(set_handler, 1.0)
-    pyglet.clock.schedule_once(remove_handler, 31.0)
-    pyglet.clock.schedule_once(delete, 31.0)
-    pyglet.clock.schedule_once(end_rou, 61.0)
-    pyglet.clock.schedule_once(set_handler, 61.0)
-    
-    trial_start = time.time()
-    
     pyglet.app.run()
-    
-    trial_end = time.time()
-    
-    # Get results
-    ku.append(trial_start + 31.0)
-    while len(kd) > 0:
-        kud.append(ku.popleft() - kd.popleft() + 0) # list up key_press_duration
-    kud_list.append(str(kud))
-    c = sum(kud)
-    cdt.append(c)
-    tcs.append(tc)
-    if kud == []:
-        kud.append(0)
-    m = np.mean(kud)
-    d = np.std(kud)
-    mdt.append(m)
-    dtstd.append(d)
-    print("--------------------------------------------------")
-    print("start: " + str(trial_start))
-    print("end: " + str(trial_end))
-    print("key_pressed: " + str(kud))
-    print("transient counts: " + str(tc))
-    print("cdt: " + str(c))
-    print("mdt: " + str(m))
-    print("dtstd "+ str(d))
-    print("condition" + str(da))
-    print("--------------------------------------------------")
+
 #-------------- End loop -------------------------------
 
 win.close()
@@ -242,7 +234,13 @@ results = pd.DataFrame({header[0]:list_a, # Store variance_A conditions
                         "dtstd":dtstd,
                         "key_press_list":kud_list}) # Store the key_press_duration list
 #index = range(ind*rept)
-results.to_csv(path_or_buf="./pr_data/" + str(daten) + ".csv", index=False) # Output experimental data
+
+if os.path.exists("data") == False:
+    os.mkdir("data")
+
+name = str(daten)
+name = name.replace(":", "'")
+results.to_csv(path_or_buf="./data/DATA" + name + ".csv", index=False) # Output experimental data
 
 # Output following to shell, check this experiment
 print(u"開始日時: " + str(start))
